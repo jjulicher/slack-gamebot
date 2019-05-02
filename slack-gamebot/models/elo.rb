527dc2e8e7601ecd47c04133f853e23d789215b5
Justin Julicher <justin.julicher@gmail.com>
2019-05-02 14:08:54 +1000

    Convert to float.

--- slack-gamebot/models/elo.rb
+++ slack-gamebot/models/elo.rb
@@ -1,8 +1,8 @@
 module Elo
   DELTA_TAU = 0.94
-  DIVISOR = ENV['ELO_DIVISOR'] if ENV.key?('ELO_DIVISOR')
-  POWER = ENV['ELO_POWER'] if ENV.key?('ELO_POWER')
-  POINTS = ENV['ELO_POINTS'] if ENV.key?('ELO_POINTS')
+  DIVISOR = (ENV['ELO_DIVISOR'] if ENV.key?('ELO_DIVISOR')).to_f
+  POWER = (ENV['ELO_POWER'] if ENV.key?('ELO_POWER')).to_f
+  POINTS = (ENV['ELO_POINTS'] if ENV.key?('ELO_POINTS')).to_f
   
   def self.team_elo(players)
     (players.sum(&:elo).to_f / players.count).round(2)
