8077abfd0709a9f9e8c92ae3621b2fff06700c34
Justin Julicher <justin.julicher@gmail.com>
2019-05-02 14:00:24 +1000

    Grab variables from environment.

--- slack-gamebot/models/elo.rb
+++ slack-gamebot/models/elo.rb
@@ -1,6 +1,9 @@
 module Elo
   DELTA_TAU = 0.94
-
+  DIVISOR = ENV['ELO_DIVISOR'] if ENV.key?('ELO_DIVISOR')
+  POWER = ENV['ELO_POWER'] if ENV.key?('ELO_POWER')
+  POINTS = ENV['ELO_POINTS'] if ENV.key?('ELO_POINTS')
+  
   def self.team_elo(players)
     (players.sum(&:elo).to_f / players.count).round(2)
   end
